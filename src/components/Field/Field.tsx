95ec391c55b2523c445f87711b90add3774c5ccf
Matthijs Groen <matthijs.groen@gmail.com>
2022-02-16 17:53:20 +0100

    Add grass texture

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,7 +1,17 @@
+import { className } from "../../utils/className";
 import styles from "./Field.module.css";
 
 export const Field: React.FC = ({ children }) => (
-  <div className={styles.field}>
-    <div className={styles.root}>{children}</div>
-  </div>
+  <>
+    <div className={styles.grass}></div>
+    <div className={styles.field}>
+      <div className={styles.root}>{children}</div>
+    </div>
+    <div
+      className={className({
+        [styles.grass]: true,
+        [styles.grassBottom]: true,
+      })}
+    ></div>
+  </>
 );
