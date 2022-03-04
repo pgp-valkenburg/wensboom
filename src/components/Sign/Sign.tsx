51457cd15966f59b0eff9830ba3376c5b77bc4be
Matthijs Groen <matthijs.groen@gmail.com>
2022-03-04 14:30:36 +0100

    Shake sign

--- src/components/Sign/Sign.tsx
+++ src/components/Sign/Sign.tsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
+import { className } from "../../utils/className";
 import styles from "./Sign.module.css";
 
-export const Sign: React.FC<{ counter: number }> = ({ counter }) => (
-  <div className={styles.sign}>
-    <h3>Wensen:</h3>
-    <p>{counter}</p>
-  </div>
-);
+export const Sign: React.FC<{ counter: number }> = ({ counter }) => {
+  const [shake, setShake] = useState(false);
+  useEffect(() => {
+    setShake(true);
+    const timer = setTimeout(() => {
+      setShake(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [counter]);
+
+  return (
+    <div className={className({ [styles.sign]: true, [styles.shake]: shake })}>
+      <h3>Wensen:</h3>
+      <p>{counter}</p>
+    </div>
+  );
+};
