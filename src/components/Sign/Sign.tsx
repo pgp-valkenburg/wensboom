452b14808c20d3b810e025084afb8654f75d8bb7
Matthijs Groen <matthijs.groen@gmail.com>
2022-03-05 08:55:51 +0100

    Add digit animation

--- src/components/Sign/Sign.tsx
+++ src/components/Sign/Sign.tsx
@@ -2,6 +2,50 @@ import { useEffect, useState } from "react";
 import { className } from "../../utils/className";
 import styles from "./Sign.module.css";
 
+const Digit: React.FC<{ char: string }> = ({ char }) => {
+  const [prevChar, setChar] = useState(" ");
+  const [flipping, setFlipping] = useState("done");
+
+  useEffect(() => {
+    setFlipping("start");
+    const timer = setTimeout(() => {
+      setChar(char);
+      setFlipping("end");
+    }, 2100);
+    return () => clearTimeout(timer);
+  }, [char]);
+
+  useEffect(() => {
+    if (flipping === "end") {
+      setFlipping("done");
+    }
+  }, [flipping]);
+
+  return (
+    <span
+      className={className({
+        [styles.digit]: true,
+        [styles.flip]: flipping === "start",
+        [styles.endFlip]: flipping === "done",
+      })}
+      data-prevchar={prevChar}
+    >
+      {char}
+    </span>
+  );
+};
+
+const Number: React.FC<{ number: number }> = ({ number }) => {
+  const chars = `${number}`.split("");
+  return (
+    <>
+      {chars.map((c, i) => (
+        <Digit key={chars.length - i} char={c} />
+      ))}
+    </>
+  );
+};
+
 export const Sign: React.FC<{ counter: number }> = ({ counter }) => {
   const [shake, setShake] = useState(false);
   useEffect(() => {
@@ -15,7 +59,9 @@ export const Sign: React.FC<{ counter: number }> = ({ counter }) => {
   return (
     <div className={className({ [styles.sign]: true, [styles.shake]: shake })}>
       <h3>Wensen:</h3>
-      <p>{counter}</p>
+      <p>
+        <Number number={counter} />
+      </p>
     </div>
   );
 };
